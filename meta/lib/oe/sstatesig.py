495064336f68828348d14fe370d2d36809bd1f89
Richard Purdie <richard.purdie@linuxfoundation.org>
2020-01-02 16:43:21 +0000

    Revert "sstatesig: Fix locked signature handling with unihashes"
    This reverts commit 3dad2445552dbae3356ce8cf3f3b752a4b3a2ede.

--- meta/lib/oe/sstatesig.py
+++ meta/lib/oe/sstatesig.py
@@ -104,7 +104,6 @@ class SignatureGeneratorOEBasicHashMixIn(object):
                                 "").split()
         self.unlockedrecipes = { k: "" for k in self.unlockedrecipes }
         self.buildarch = data.getVar('BUILD_ARCH')
-        self._internal = False
         pass
 
     def tasks_resolved(self, virtmap, virtpnmap, dataCache):
@@ -157,12 +156,7 @@ class SignatureGeneratorOEBasicHashMixIn(object):
             else:
                 return super().get_taskhash(tid, deps, dataCache)
 
-        # get_taskhash will call get_unihash internally in the parent class, we 
-        # need to disable our filter of it whilst this runs else
-        # incorrect hashes can be calculated.
-        self._internal = True
         h = super().get_taskhash(tid, deps, dataCache)
-        self._internal = False
 
         (mc, _, task, fn) = bb.runqueue.split_tid_mcfn(tid)
 
@@ -205,7 +199,7 @@ class SignatureGeneratorOEBasicHashMixIn(object):
         return h
 
     def get_unihash(self, tid):
-        if tid in self.lockedhashes and self.lockedhashes[tid] and not self._internal:
+        if tid in self.lockedhashes and self.lockedhashes[tid]:
             return self.lockedhashes[tid]
         return super().get_unihash(tid)
 
